6f57d8a5dd22b7d790c2eca2ad7dfc06f956f8f9
TobyFlan <toby.flanagan@hotmail.co.uk>
2024-11-04 13:25:42 +0000

    fix tailwind error and add navbar

---
+++ blogapp/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+export default function Navbar() {
+    const user = null;
+    const username = null;
+    return (
+        <nav className="bg-white shadow dark:bg-gray-800">
+            <div className="container mx-auto px-5 py-4">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center">
+                        <Link href="/" className="text-xl font-semibold text-gray-800 dark:text-white">
+                            Logo
+                        </Link>
+                    </div>
+                    <div className="flex items-center space-x-4">
+                        <Button asChild variant="ghost">
+                            <Link href="/">Feed</Link>
+                        </Button>
+                        {username ? (
+                            <>
+                                <Button asChild variant="ghost">
+                                <Link href="/admin">Write Posts</Link>
+                                </Button>
+                                <Link href={`/${username}`}>
+                                <Avatar>
+                                    <AvatarImage src={user?.photoURL} alt="Profile" />
+                                    <AvatarFallback>{username[0]}</AvatarFallback>
+                                </Avatar>
+                                </Link>
+                            </>
+                            ) : (
+                            <Button asChild variant="default">
+                                <Link href="/enter">Log in</Link>
+                            </Button>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
--- blogapp/src/pages/index.tsx
+++ blogapp/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
+import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <title key="title">Home</title>
     </Head>
       <div>
+        <Loader show={true} />
         <h1>Home</h1>
         <Link prefetch={false} href={`/user`}>
           User&apos;s profile
